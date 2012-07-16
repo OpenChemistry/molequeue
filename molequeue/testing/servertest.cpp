8ea4f1ff77d7258c90d5a5fc9291ff8259148331
Chris Harris <chris.harris@kitware.com>
2012-07-16 12:56:36 -0400

    Use generated connection string for server tests
    Ensure server test will run in parallel correctly by used generated
    connection string rather than hardcoded "MoleQueue-testing".
    
    Change-Id: I7f0ef3aaadb681284ed15a1bbdd5aaf419ad19be

--- molequeue/testing/servertest.cpp
+++ molequeue/testing/servertest.cpp
@@ -21,6 +21,7 @@
 #include "molequeueglobal.h"
 #include "transport/connectionlistener.h"
 #include "transport/localsocket/localsocketconnectionlistener.h"
+#include "testing/testserver.h"
 
 #include <QtGui/QApplication>
 
@@ -36,6 +37,7 @@ class ServerTest : public QObject
   Q_OBJECT
 
 private:
+  QString m_connectionString;
   QLocalSocket m_testSocket;
   MoleQueue::Server *m_server;
 
@@ -61,14 +63,15 @@ private slots:
 
 void ServerTest::initTestCase()
 {
-  m_server = new MoleQueue::Server(this, QString("MoleQueue-testing"));
+  m_connectionString = TestServer::getRandomSocketName();
+  m_server = new MoleQueue::Server(this, m_connectionString);
   m_server->m_isTesting = true;
   m_server->setDebug(true);
 }
 
 void ServerTest::cleanupTestCase()
 {
-  delete m_server;
+
 }
 
 void ServerTest::init()
@@ -86,8 +89,6 @@ MoleQueue::LocalSocketConnectionListener *
 {
 
   MoleQueue::LocalSocketConnectionListener *localListener = NULL;
-     static_cast<MoleQueue::LocalSocketConnectionListener *>(
-          m_server->m_connectionListeners.at(0));
 
   foreach(MoleQueue::ConnectionListener *listener,
           m_server->m_connectionListeners) {
@@ -120,7 +121,7 @@ void ServerTest::testStop()
 void ServerTest::testForceStart()
 {
   // Start a duplicate server to take the socket address
-  MoleQueue::Server dupServer(this, QString("MoleQueue-testing"));
+  MoleQueue::Server dupServer(this, m_connectionString);
   dupServer.m_isTesting = true;
   dupServer.setDebug(true);
   dupServer.start();
@@ -156,7 +157,7 @@ void ServerTest::testNewConnection()
   m_server->stop();
   m_server->start();
 
-  m_testSocket.connectToServer("MoleQueue-testing");
+  m_testSocket.connectToServer(m_connectionString);
   qApp->processEvents(QEventLoop::AllEvents, 1000);
   QVERIFY(m_testSocket.state() == QLocalSocket::ConnectedState);
 
@@ -171,7 +172,6 @@ void ServerTest::testNewConnection()
 
 void ServerTest::testClientDisconnected()
 {
-
 #ifdef USE_ZERO_MQ
   QCOMPARE(m_server->m_connections.size(), 2);
 #else
