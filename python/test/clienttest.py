a047fe167a598adfccc70268a01829b8bcdb11e1
Chris Harris <chris.harris@kitware.com>
2012-08-08 15:31:26 -0400

    Convert test case to Python unittest.TestCase
    Use Python's testing framework
    
    Change-Id: I924f329c43294bb1fbb395a7bb19bcaf06ea9385

--- python/test/clienttest.py
+++ python/test/clienttest.py
@@ -1,10 +1,18 @@
+import unittest
+
 import molequeue
 
-client = molequeue.Client()
-client.connect_to_server('MoleQueue')
+class TestClient(unittest.TestCase):
 
-job_request = molequeue.JobRequest()
-job_request.queue = 'salix'
-job_request.program = 'sleep (testing)'
+  def test_submit_job_request(self):
+    client = molequeue.Client()
+    client.connect_to_server('MoleQueue')
 
-client.submit_job_request(job_request)
+    job_request = molequeue.JobRequest()
+    job_request.queue = 'salix'
+    job_request.program = 'sleep (testing)'
+
+    client.submit_job_request(job_request)
+
+if __name__ == '__main__':
+    unittest.main()
