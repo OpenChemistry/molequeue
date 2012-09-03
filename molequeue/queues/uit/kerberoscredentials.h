1f7c1db24ef143410c14fd836a4aa48859b1379f
Chris Harris <chris.harris@kitware.com>
2012-09-03 11:20:55 -0400

    Add support for ezHPC UIT.
    Add support to enable authentication with UIT as a first step to
    provide support for submitting jobs using UIT.
    
    Change-Id: I63156097a8c0f87a4ab12bf01813150a1dbe83ac

---
+++ molequeue/queues/uit/kerberoscredentials.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+ This source file is part of the MoleQueue project.
+
+ Copyright 2012 Kitware, Inc.
+
+ This source code is released under the New BSD License, (the "License").
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ ******************************************************************************/
+
+#ifndef KERBEROSCREDENTIALS_H_
+#define KERBEROSCREDENTIALS_H_
+
+#include <QtCore/QString>
+
+namespace MoleQueue
+{
+/**
+ * @brief class used to model UIT KerberosCredentials
+ */
+class KerberosCredentials
+{
+public:
+
+  /**
+   * @param principle The Kerberos username.
+   * @param password The Kerberos password.
+   */
+  KerberosCredentials(const QString &principle, const QString &password);
+
+  /**
+   * @return The XML message for this KerberosCredentials instance.
+   */
+  QString toXml() const;
+
+private:
+  QString m_principle;
+  QString m_password;
+};
+
+} /* namespace MoleQueue */
+
+#endif /* KERBEROSCREDENTIALS_H_ */
