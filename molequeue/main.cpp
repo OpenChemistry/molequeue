0f6ee5f6917aa88b952843eaf5500d4825c1e189
David C. Lonie <david.lonie@kitware.com>
2012-10-04 15:55:39 -0400

    Add commandline option for running in a custom workdir.
    Also add basic help and version options.
    
    Change-Id: Iba67b7f174c637794bff266c252fffd6d09934bf

--- molequeue/main.cpp
+++ molequeue/main.cpp
@@ -14,11 +14,21 @@
 
 ******************************************************************************/
 
+#include <QtCore/QDir>
+#include <QtCore/QSettings>
+#include <QtCore/QStringList>
+
 #include <QtGui/QApplication>
 #include <QtGui/QSystemTrayIcon>
 #include <QtGui/QMessageBox>
 
 #include "mainwindow.h"
+
+#include <cstdio>
+
+void printVersion();
+void printUsage();
+bool setWorkDir(const QString &workDir);
 
 int main(int argc, char *argv[])
 {
@@ -29,10 +39,42 @@ int main(int argc, char *argv[])
   
   QApplication app(argc, argv);
 
+  bool customWorkDirSet = false;
+  QStringList args = QCoreApplication::arguments();
+  for (QStringList::const_iterator it = args.constBegin() + 1,
+       it_end = args.constEnd(); it != it_end; ++it) {
+    if (*it == "-w" || *it == "--workdir") {
+      if (customWorkDirSet) {
+        printf("More than one -w or -W option set. Cannot run in multiple "
+               "working directories!\n");
+        return EXIT_FAILURE;
+      }
+      if (!setWorkDir(it + 1 == it_end ? QString("") : *(++it))) {
+        return EXIT_FAILURE;
+      }
+      customWorkDirSet = true;
+      continue;
+    }
+    else if (*it == "-v" || *it == "--version") {
+      printVersion();
+      return EXIT_SUCCESS;
+    }
+    else if (*it == "-h" || *it == "-H" || *it == "--help" || *it == "-help") {
+      printUsage();
+      return EXIT_SUCCESS;
+    }
+    else {
+      QByteArray ba = it->toLatin1();
+      printf("Unrecognized command line option: '%s'\n", ba.constData());
+      printUsage();
+      return EXIT_FAILURE;
+    }
+  }
+
   if (!QSystemTrayIcon::isSystemTrayAvailable()) {
     QMessageBox::critical(0, QObject::tr("MoleQueue"),
                           QObject::tr("System tray not available on this system."));
-    return 1;
+    return EXIT_FAILURE;
   }
 
   QApplication::setQuitOnLastWindowClosed(false);
@@ -40,4 +82,82 @@ int main(int argc, char *argv[])
   MoleQueue::MainWindow window;
   window.show();
   return app.exec();
+}
+
+void printVersion()
+{
+  QByteArray appName = QCoreApplication::applicationName().toLatin1();
+  QByteArray ba = QCoreApplication::applicationVersion().toLatin1();
+  printf("%s %s\n", appName.constData(), ba.constData());
+}
+
+void printUsage()
+{
+  printVersion();
+  printf("Usage: molequeue [options]\n\nOptions:\n");
+
+  const char *fmt = "      %3s %-20s   %s\n";
+
+  printf(fmt, "-h,", "--help",
+         "Print version and usage information and exit.");
+  printf(fmt, "-v,", "--version",
+         "Print version information and exit.");
+  printf(fmt, "-w,", "--workdir [path]",
+         "Run MoleQueue in a custom working directory.");
+}
+
+bool setWorkDir(const QString &workDir)
+{
+  QByteArray ba = workDir.toLatin1();
+
+  QFileInfo dirInfo(workDir);
+  if (!dirInfo.exists()) {
+    printf("Specified working directory does not exist: '%s'\n",
+           ba.constData());
+    return false;
+  }
+  if (!dirInfo.isReadable()) {
+    printf("Specified working directory is not readable: '%s'\n",
+           ba.constData());
+    return false;
+  }
+  if (!dirInfo.isWritable()) {
+    printf("Specified working directory is not writable: '%s'\n",
+           ba.constData());
+    return false;
+  }
+
+  QDir dir(workDir);
+
+  if (dir.exists("config")) {
+    QFileInfo configInfo(dir, "config");
+    if (!configInfo.isDir()) {
+      printf("Invalid working directory '%s': '%s/config' exists and is not a "
+             "directory!\n", ba.constData(), ba.constData());
+      return false;
+    }
+    if (!configInfo.isReadable()) {
+      printf("Invalid working directory '%s': '%s/config' is not readable!\n",
+             ba.constData(), ba.constData());
+      return false;
+    }
+    if (!configInfo.isWritable()) {
+      printf("Invalid working directory '%s': '%s/config' is not writable!\n",
+             ba.constData(), ba.constData());
+      return false;
+    }
+  }
+  else {
+    if (!dir.mkdir("config")) {
+      printf("Cannot create directory '%s'. Aborting.", ba.constData());
+      return false;
+    }
+  }
+
+  QSettings::setPath(QSettings::NativeFormat, QSettings::UserScope,
+                     workDir + "/config/");
+  QSettings settings;
+  settings.setValue("workingDirectoryBase", workDir);
+
+  return true;
 }
