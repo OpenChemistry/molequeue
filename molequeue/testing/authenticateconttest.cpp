9f66da944781fd19d124e6c61ce612f455c6c61c
Chris Harris <chris.harris@kitware.com>
2012-10-09 16:58:23 -0400

    Add test cases for UIT authentication
    Change-Id: I466d1a9372151a97b02114fe9da37dfb9421015c

---
+++ molequeue/testing/authenticateconttest.cpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+
+ This source file is part of the MoleQueue project.
+
+ Copyright 2012 Kitware, Inc.
+
+ This source code is released under the New BSD License, (the "License").
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ ******************************************************************************/
+
+#include <QtTest>
+#include <QtXmlPatterns/QXmlQuery>
+
+#include "queues/uit/authenticatecont.h"
+#include "queues/uit/authenticateresponse.h"
+#include "referencestring.h"
+#include "xmlutils.h"
+
+class AuthenticateContTest : public QObject
+{
+  Q_OBJECT
+private slots:
+  void testToXmlNoReply();
+  void testToXmlWithReply();
+};
+
+void AuthenticateContTest::testToXmlNoReply()
+{
+  QList<MoleQueue::Prompt> prompts;
+  prompts << MoleQueue::Prompt(0, "prompt1");
+  prompts << MoleQueue::Prompt(1, "prompt2");
+
+  QString id = "sessionId";
+
+  MoleQueue::AuthenticateCont authCont(id, prompts);
+
+  ReferenceString expected(
+    "authenticatecont-ref/authenticatecont-no-reply.xml");
+  QCOMPARE(authCont.toXml(), XmlUtils::stripWhitespace(expected));
+
+}
+
+void AuthenticateContTest::testToXmlWithReply()
+{
+  QList<MoleQueue::Prompt> prompts;
+  MoleQueue::Prompt prompt1(0, "prompt1");
+  prompt1.setUserResponse("reply1");
+  MoleQueue::Prompt prompt2(1, "prompt2");
+  prompt2.setUserResponse("reply2");
+
+  prompts << prompt1 << prompt2;
+
+  QString id = "sessionId";
+
+  MoleQueue::AuthenticateCont authCont(id, prompts);
+
+  ReferenceString expected(
+    "authenticatecont-ref/authenticatecont-reply.xml");
+  QCOMPARE(authCont.toXml(), XmlUtils::stripWhitespace(expected));
+
+}
+
+
+QTEST_MAIN(AuthenticateContTest)
+
+#include "authenticateconttest.moc"
---
+++ molequeue/testing/kerberoscredentialstest.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+ This source file is part of the MoleQueue project.
+
+ Copyright 2012 Kitware, Inc.
+
+ This source code is released under the New BSD License, (the "License").
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ ******************************************************************************/
+
+#include <QtTest>
+
+#include "queues/uit/kerberoscredentials.h"
+#include "referencestring.h"
+#include "xmlutils.h"
+
+class KerberosCredentialsTest : public QObject
+{
+  Q_OBJECT
+private slots:
+  void testToXml();
+
+};
+
+
+void KerberosCredentialsTest::testToXml()
+{
+  ReferenceString expected("kerberoscredentials-ref/kerberoscredentials.xml");
+
+  MoleQueue::KerberosCredentials credentials("test", "test");
+  QCOMPARE(credentials.toXml(),
+           XmlUtils::stripWhitespace(expected));
+}
+
+QTEST_MAIN(KerberosCredentialsTest)
+
+#include "kerberoscredentialstest.moc"
