1e50f9a977c1caa9822022db7987d6bc48f80d4e
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-10-14 17:38:22 -0400

    Add a few comments about where it comes from
    Also only build the executable if the variable is injected into the
    cache, it provides some basic manual verification of core functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,25 @@ int main(int argc, char *argv[])
   json.insert("params", params);
   json.insert("id", QJsonValue(42));
 
+  json["myKey"] = 5;
+  json["myString"] = QLatin1String("Test");
+
   QJsonArray array;
   array.append(2);
   array.append(3);
   array.append(5);
   array.append(7);
   array.append(11);
+  array.append(QString("Aaaarrrrgghhhhh"));
   json.insert("primes", array);
 
   QJsonDocument doc(json);
   qDebug() << doc.toJson();
+
+  QJsonDocument doc2;
+  doc2 = QJsonDocument::fromJson(doc.toJson());
+
+  qDebug() << "Other JSON:\n" << doc2.toJson();
 
   return 0;
 }
