35cfb3f1fc0b4e2b1706a5698ed9a688813c004f
David C. Lonie <david.lonie@kitware.com>
2012-10-17 13:41:21 -0400

    Add basic test for client/server interaction.
    This patch adds a test that starts the MoleQueue application
    in a clean working directory on a random socket, and then
    runs a series of python scripts to:
    * submit one job, wait for it to finish
    * submit 200 jobs, wait for them to finish
    * submit 200 jobs from 10 clients simultaneously, wait for them to finish
    * kill the molequeue server application
    
    Change-Id: Ibd4ace3a8c3338bb6400b3cd98c8ce79798a1e82

---
+++ molequeue/testing/scripts/sendRpcKill.py
@@ -0,0 +1,43 @@
+import molequeue as mq
+from getopt import getopt
+import sys
+import time
+
+debug = False
+
+socketname = ""
+
+def run_test():
+  global socketname
+  if debug:
+    print "Connecting to socket: %s"%socketname
+  client = mq.Client()
+  client.connect_to_server(socketname)
+  res = client._send_rpc_kill_request(5)
+  client.disconnect()
+  if res == None:
+    raise Exception("Connection timed out!")
+  if res != True:
+    return 1
+  return 0
+
+def main(argv):
+  # -s for socketname
+  opts, args = getopt(argv, "s:")
+  for opt, arg in opts:
+    if opt == "-s":
+      global socketname
+      socketname = arg
+
+  try:
+    test_result = run_test()
+  except Exception as ex:
+    print ex
+    test_result = 1
+  if debug:
+    print "Exiting with code: %d"%test_result
+  sys.stdout.flush()
+  return test_result
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv[1:]))
