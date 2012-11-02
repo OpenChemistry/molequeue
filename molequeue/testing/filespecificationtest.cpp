7e0ed50934dac0145db1aa3ac16dceba54ca18bc
David C. Lonie <david.lonie@kitware.com>
2012-11-02 11:32:18 -0400

    WIP: Misc windows fixes:
    * Platform dependent path fixes
    * Changed QVERIFY to QCOMPARE to get a bit more detail about
      a failure.
    * Force start the connection in a failing server test.
    
    Change-Id: I4680e28a258ee02240f4c74d3addf4da35124a20

--- molequeue/testing/filespecificationtest.cpp
+++ molequeue/testing/filespecificationtest.cpp
@@ -266,7 +266,11 @@ void FileSpecificationTest::contents()
 void FileSpecificationTest::filepath()
 {
   FileSpecification pathSpec(QString("/path/to/some/file.ext"));
+#ifdef WIN32
+  QCOMPARE(pathSpec.filepath(), QString("C:/path/to/some/file.ext"));
+#else
   QCOMPARE(pathSpec.filepath(), QString("/path/to/some/file.ext"));
+#endif
 
   FileSpecification contSpec("file.ext", "contents\n");
   QVERIFY(contSpec.filepath().isNull());
