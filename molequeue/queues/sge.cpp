d8a540bd413e6b9d52082258f105410323f7c033
David C. Lonie <david.lonie@kitware.com>
2013-03-28 16:27:46 -0400

    Comment out the -pe sge line so it will work out of a box.
    Change-Id: I7d1caf03c7601a340ca83a1ad77cc781d52c16d0

--- molequeue/queues/sge.cpp
+++ molequeue/queues/sge.cpp
@@ -38,7 +38,7 @@ QueueSge::QueueSge(QueueManager *parentManager) :
       "#\n"
       "#$ -S /bin/bash\n"
       "#$ -N MoleQueueJob-$$moleQueueId$$\n"
-      "#$ -pe NODETYPE $$numberOfCores$$\n"
+      "##$ -pe NODETYPE $$numberOfCores$$\n"
       "#$ -l h_rt=$$maxWallTime$$\n"
       "\n"
       "cd $SGE_O_WORKDIR\n"
