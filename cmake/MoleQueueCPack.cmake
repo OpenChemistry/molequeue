97c88029069eb94b1488632660b13671c7938243
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-04-04 20:50:42 -0400

    Customize CPack a little, get NSIS working
    We want CPack to use our version, rather than the default 0.1.1, add
    the license file and icon setup along with some useful links once
    everything is installed.
    
    Change-Id: Ib1183f628f8bcc642e3ecd3ab843f7fad7839d48

---
+++ cmake/MoleQueueCPack.cmake
@@ -0,0 +1,17 @@
+set(CPACK_PACKAGE_NAME "MoleQueue")
+set(CPACK_PACKAGE_VERSION_MAJOR ${MoleQueue_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${MoleQueue_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${MoleQueue_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION ${MoleQueue_VERSION})
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "MoleQueue")
+set(CPACK_RESOURCE_FILE_LICENSE "${MoleQueue_SOURCE_DIR}/COPYING")
+
+set(CPACK_PACKAGE_EXECUTABLES "molequeue" "MoleQueue")
+set(CPACK_CREATE_DESKTOP_LINKS "molequeue")
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/MoleQueueCPackOptions.cmake.in"
+  "${MoleQueue_BINARY_DIR}/MoleQueueCPackOptions.cmake" @ONLY)
+set(CPACK_PROJECT_CONFIG_FILE
+  "${MoleQueue_BINARY_DIR}/MoleQueueCPackOptions.cmake")
+
+include(CPack)
