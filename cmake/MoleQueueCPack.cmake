a240614369e809e581f963aa8e4aad71b2d4870e
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2018-01-31 10:35:43 -0500

    Move the COPYING file to LICENSE

--- cmake/MoleQueueCPack.cmake
+++ cmake/MoleQueueCPack.cmake
@@ -9,14 +9,14 @@ set(CPACK_PACKAGE_DESCRIPTION
   "Desktop integration of high performance computing resources.")
 
 if(APPLE)
-  configure_file("${MoleQueue_SOURCE_DIR}/COPYING"
-    "${MoleQueue_BINARY_DIR}/COPYING.txt" @ONLY)
-  set(CPACK_RESOURCE_FILE_LICENSE "${MoleQueue_BINARY_DIR}/COPYING.txt")
+  configure_file("${MoleQueue_SOURCE_DIR}/LICENSE"
+    "${MoleQueue_BINARY_DIR}/LICENSE.txt" @ONLY)
+  set(CPACK_RESOURCE_FILE_LICENSE "${MoleQueue_BINARY_DIR}/LICENSE.txt")
   set(CPACK_PACKAGE_ICON
     "${MoleQueue_SOURCE_DIR}/molequeue/app/icons/molequeue.icns")
   set(CPACK_BUNDLE_ICON "${CPACK_PACKAGE_ICON}")
 else()
-  set(CPACK_RESOURCE_FILE_LICENSE "${MoleQueue_SOURCE_DIR}/COPYING")
+  set(CPACK_RESOURCE_FILE_LICENSE "${MoleQueue_SOURCE_DIR}/LICENSE")
 endif()
 
 set(CPACK_PACKAGE_EXECUTABLES "molequeue" "MoleQueue")
